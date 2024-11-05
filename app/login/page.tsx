5dc1a08ac8356654d7267e19b9185954bc42cdab
Moacir <moacir.1000rr@hotmail.com>
2024-11-05 18:01:46 -0300

    feat: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/dist/server/api-utils";
 
-const LoginPage  = () => {
+const LoginPage  = async () => {
+    const { userId } = await auth();
+    if (userId) {
+        redirect("/");
+
+    }
+
     return (
         <div className="grid h-full grid-cols-2">
             
@@ -12,10 +21,13 @@ const LoginPage  = () => {
             <h1 className="text-4xl font-bold mb-3">Bem-vindo</h1>
 
             <p className="text-muted-foreground mb-8" >A Finance AI é uma plataforma de gestão financeira que utiliza IA para monitorar suas movimentações, e oferecer insights personalizados, facilitando o controle do seu orçamento.</p>
+            <SignInButton>
             <Button variant={"outline"}>
                 <LogInIcon className="mr-2"/>
                 Fazer login ou criar
             </Button>
+            </SignInButton>
+            
 
             </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
-const Home = () => {
-return <h1 className="text-red-500 p-5"> Hello World</h1>
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/dist/server/api-utils";
 
+const Home = async () => {
+    const{ userId } = await auth();
+    if (!userId) {
+        redirect("/login")
+    }
+
+return (<div className="h-full flex items-center justify-center">
+        <UserButton showName />
+        </div>
+        );
 };
 
 export default Home;
